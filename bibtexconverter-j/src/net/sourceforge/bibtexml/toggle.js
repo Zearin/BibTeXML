32c4faef31310b1d38b608f8ae779135c51fbfa1
ringler <ringler@eef7465d-6424-0410-b864-a94a441bc073>
2007-03-15 17:04:19 +0000

    moved xslt stylesheets and related resources to xslt folder

--- bibtexconverter-j/src/net/sourceforge/bibtexml/toggle.js
+++
@@ -1,8 +0,0 @@
-function toggleAbstract(citekey){
-  abs = eval(document.getElementById("abstract_"+citekey));
-  if(abs.style.display=="none"){
-      abs.style.display="block";
-  } else {
-    abs.style.display="none";
-  }
-}
