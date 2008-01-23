845624301aeb61434440c421399f0bea79d011ec
ringler <ringler@eef7465d-6424-0410-b864-a94a441bc073>
2008-01-23 09:06:27 +0000

    parse dependent aux files included with \@input

--- bibtexconverter-j/src/net/sourceforge/bibtexml/AuxParser.java
+++ bibtexconverter-j/src/net/sourceforge/bibtexml/AuxParser.java
@@ -20,51 +20,114 @@ package net.sourceforge.bibtexml;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 import java.io.*;
+import java.nio.charset.Charset;
 import java.util.*;
 import java.net.URL;
 import java.net.MalformedURLException;
 
 public class AuxParser {
-    private List<String> citations = new ArrayList<String>();
+    private final List<String> citations = new ArrayList<String>();
+    private final Set<URL> urlCache = new HashSet<URL>();
+    private final URL baseurl;
+    private final Charset charset;
 
-    public AuxParser(){
-        //default constructor
+    public AuxParser(URL base, Charset chars){
+        baseurl = base;
+        charset = chars;
     }
 
     public void reset(){
         citations.clear();
-    }
-
-    public void parse(BufferedReader reader) throws IOException{
-        Matcher m = Pattern.compile("\\s*\\\\citation\\{(.*)\\}\\s*").matcher("");
-        for(String line = reader.readLine();
-            line != null;
-            line = reader.readLine())
-        {
-            if(m.reset(line).matches()){
-                final String[] elements = m.group(1).split(",");
-                for(String citation : elements){
-                    final String ct = citation.trim();
-                    if(!citations.contains(ct)){//report only first citation
-                        citations.add(ct);
-                    }
-                }
-            }
-        }
+        urlCache.clear();
     }
 
     public String[] getCitations(){
         return citations.toArray(new String[citations.size()]);
     }
 
+    public void parse(BufferedReader reader) throws IOException, MalformedURLException{
+        final Matcher mCite =
+            Pattern.compile("\\s*\\\\citation\\{(.*)\\}\\s*").matcher("");
+        final Matcher mInclude =
+            Pattern.compile("\\s*\\\\@input\\{(.*)\\}\\s*").matcher("");
+        for(String line = reader.readLine();
+            line != null;
+            line = reader.readLine())
+        {
+            if(mCite.reset(line).matches()){
+                final String[] elements = mCite.group(1).split(",");
+                for(String citation : elements){
+                    final String ct = citation.trim();
+                    if(!citations.contains(ct)){//report only first citation
+                        citations.add(ct);
+                    }
+                }
+            } else if (mInclude.reset(line).matches()){
+                parse(asURL(baseurl, mInclude.group(1).trim()));
+            }
+        }
+    }
+
+    public void parse(URL url) throws IOException{
+        //we try to avoid infinite recursion by checking if
+        //we have already accessed the specified url
+        if(!urlCache.contains(url)){
+            urlCache.add(url);
+            final BufferedReader reader = new BufferedReader(
+                new InputStreamReader(url.openStream(), charset));
+            try{
+                parse(reader);
+            } finally {
+                reader.close();
+            }
+        }
+    }
+
+    private static URL asURL(URL url, String path) throws MalformedURLException{
+        URL result = null;
+        try{
+            final File f = new File(path);
+            if(f != null && f.isFile()){
+                result = f.toURI().toURL();
+            }
+        } catch (Exception ignore){
+        }
+        if(result == null){
+            if(url == null){
+                result = new URL(path);
+            } else {
+                result = new URL(url, path);
+            }
+        }
+        return result;
+    }
+
+    public static String[] parse(URL auxfile, Charset encoding)
+            throws IOException, MalformedURLException{
+        String[] result = null;
+        if (auxfile != null){
+            final AuxParser parser = new AuxParser(auxfile, encoding);
+            parser.parse(auxfile);
+            result = parser.getCitations();
+        }
+        return result;
+    }
+
+    /** This method is meant to be used from an XSLT stylesheet. It tries
+        to construct an URL from auxfile and a charset from encoding
+        and then calls {@link parse(URL, Charset)}. Errors are handled
+        by writing a message to the standard error stream. Java applications
+        should use {@link parse(URL, Charset)} instead.
+        @deprecated This method should not be used by Java applications
+     **/
     public static String[] xsltParse(String auxfile, String encoding){
         String[] result = null;
         try{
-            result = parse(auxfile, encoding);
+            result = parse(asURL(null, auxfile), Charset.forName(encoding));
         } catch (MalformedURLException x){
-            System.err.println("Cannot open aux file " + auxfile);
+            System.err.println("Cannot open aux file " + x);
         } catch (IOException x){
-            System.err.println("Error reading from  " + auxfile);
+            System.err.println("Cannot read file");
             System.err.println(x);
         } catch (Exception x){
             System.err.println(x);
@@ -72,38 +135,9 @@ public class AuxParser {
         return result;
     }
 
-    public static String[] parse(String auxfile, String encoding) throws IOException,
-            UnsupportedEncodingException, MalformedURLException{
-        String[] result = null;
-        if (auxfile != null){
-            String url = auxfile;
-            //if auxfile is a file path, convert to URL
-            try{
-                File f = new File(auxfile);
-                if(f != null && f.isFile()){
-                    url = f.toURI().toURL().toString();
-                }
-            } catch (Exception ignore){
-            }
-            //open auxfile for reading
-            BufferedReader reader = new BufferedReader(
-                new InputStreamReader((new URL(url)).openStream(),
-                    encoding));
-            try{
-                AuxParser parser = new AuxParser();
-                parser.parse(reader);
-                result = parser.getCitations();
-            } finally {
-                reader.close();
-            }
-        }
-        return result;
-    }
-
-    public static void main(String[] argv) throws Exception{
+    public static void main(String[] argv){
         for(String arg : argv){
-            String url = (new File(arg)).toURI().toURL().toString();
-            System.out.println(Arrays.asList(parse(url, "iso-8859-1")));
+            System.out.println(Arrays.asList(xsltParse(arg, "iso-8859-1")));
         }
     }
 }
