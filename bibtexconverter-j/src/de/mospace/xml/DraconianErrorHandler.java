2d5ccbbe797c0a6ae3416207a745760be7ce251a
ringler <ringler@eef7465d-6424-0410-b864-a94a441bc073>
2011-01-19 19:19:39 +0000

    Updating de.mospace.* (source level 1.6)

--- bibtexconverter-j/src/de/mospace/xml/DraconianErrorHandler.java
+++ bibtexconverter-j/src/de/mospace/xml/DraconianErrorHandler.java
@@ -4,34 +4,38 @@ import org.xml.sax.SAXParseException;
 import org.xml.sax.SAXException;
 
 /** The first errorhandler my be null, the second may not. */
-public final class DraconianErrorHandler implements ResettableErrorHandler{
+public final class DraconianErrorHandler implements ResettableErrorHandler {
     private static ResettableErrorHandler instance;
 
-    private DraconianErrorHandler(){
-        //sole constructor
+    private DraconianErrorHandler() {
+        // sole constructor
     }
 
-    public static synchronized ResettableErrorHandler getInstance(){
-        if(instance == null){
+    public static synchronized ResettableErrorHandler getInstance() {
+        if (instance == null) {
             instance = new DraconianErrorHandler();
         }
         return instance;
     }
 
-    public synchronized void fatalError( SAXParseException e ) throws SAXException {
+    @Override
+    public synchronized void fatalError(SAXParseException e)
+            throws SAXException {
         throw e;
     }
 
-    public synchronized void error( SAXParseException e ) throws SAXException {
+    @Override
+    public synchronized void error(SAXParseException e) throws SAXException {
         throw e;
     }
 
-    public synchronized void warning( SAXParseException e ) throws SAXException {
+    @Override
+    public synchronized void warning(SAXParseException e) throws SAXException {
         throw e;
     }
 
-    public synchronized void reset(){
-
+    @Override
+    public synchronized void reset() {
+        // do nothing
     }
-
 }
